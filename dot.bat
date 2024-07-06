8e97794a5f904a573b88125740d52ca1c1cea012
Asd22 Entertainment <158327205+neoapps-dev@users.noreply.github.com>
2024-07-06 14:18:51 +0300

    Add files via upload

---
+++ dot.bat
@@ -0,0 +1,98 @@
+@echo off
+setlocal EnableDelayedExpansion
+for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a"
+set version=1
+::    __  __             _                    _  _    _        ____          _     _____  _       _____ 
+::   |  \/  |           | |                  (_)| |  | |      |  _ \        | |   / ____|| |     |_   _|
+::   | \  / |  __ _   __| |  ___   __      __ _ | |_ | |__    | |_) |  __ _ | |_ | |     | |       | |  
+::   | |\/| | / _` | / _` | / _ \  \ \ /\ / /| || __|| '_ \   |  _ <  / _` || __|| |     | |       | |  
+::   | |  | || (_| || (_| ||  __/   \ V  V / | || |_ | | | |  | |_) || (_| || |_ | |____ | |____  _| |_ 
+::   |_|  |_| \__,_| \__,_| \___|    \_/\_/  |_| \__||_| |_|  |____/  \__,_| \__| \_____||______||_____|
+::                                                                                                      
+::                                                                                                      
+
+:: dotlang, (C) 2024 NEOAPPS
+:: This code is licensed under MIT license (see LICENSE.txt for details)
+
+if /i "%1" == "--help" (goto :help)
+if /i "%1" == "/?" (goto :help)
+if /i "%1" == "-h" (goto :help)
+if /i "%1" == "build" (goto :build)
+if /i "%1" == "new" (goto :new)
+if /i "%1" == "add" (goto :add)
+if /i "%1" == "idk" (echo it's alright. idk too hehe && exit /b 0)
+echo Invalid Syntax! use `dot --help` for help.
+goto :EOF
+
+:help
+echo dotCLI - The CLI for dotlang.
+echo Current dot Version Installed: dot v%version%.
+echo.
+echo Usage:
+echo %\e%[4mdot%\e%[0m --help, -h, /?		Show help Page
+echo %\e%[4mdot%\e%[0m build				Build Project in the current directory
+echo %\e%[4mdot%\e%[0m new					Make a project in the current directory
+echo %\e%[4mdot%\e%[0m add PACKAGE          Adds PACKAGE to the current project
+exit /b
+
+:new
+if exist "%cd%\dot.json" (
+echo %\e%[1;31mWARNING: PROJECT IN THIS DIRECTORY ALREADY EXIST. IF YOU WANT TO OVERRIDE IT, MAKE A FILE WITH NAME "override.txt".%\e%[0m
+if exist override.txt (
+echo {"dotv":"%version%", "name":"mydot", "v": "0.1", "packages": []} >dot.json
+del override.txt
+echo Project with name `mydot` has been made in %cd%.
+)
+) else (
+echo {"dotv":"%version%", "name":"mydot", "v": "0.1", "packages": []} >dot.json
+echo Import-Module .\addons.ps1 # Enable dotlang >main.dot
+echo Project with name `mydot` has been made in %cd%.
+)
+goto :EOF
+
+:build
+echo [dot.build] Reinitialized build.log file.>build.log
+echo [dot] Building project from %cd%...
+echo [dot] Building project from %cd%... >>build.log
+%SystemRoot%\system32\timeout 1 >nul
+type dot.json | %~dp0\jq .dotv >temp
+set /p dotv=<temp
+del temp
+if "%version%" LSS %dotv% (echo [dot] You're using an outdated version of dot. please install dot %dotv% or more && exit /b 1)
+echo [dot] Getting the latest packages from %cd%\dot.json...
+echo [dot] Getting the latest packages from %cd%\dot.json... >>build.log
+for /f "tokens=*" %%i in ('jq -r ".packages[]" dot.json') do (
+    set "child=%%i"
+    echo [dot] Running: ship install !child!
+    echo [dot] Running: ship install !child! >>build.log
+    echo [dot] Begin `ship install !child!` log.. >>build.log
+    call ship install !child! >>build.log
+    echo [dot] End `ship intall !child!` log.. >>build.log
+    %SystemRoot%\system32\timeout 2 >nul
+)
+echo [dot] Building project...
+echo [dot] Building project... >>build.log
+del dist
+mkdir dist >>build.log
+xcopy /Y *.dot dist >>build.log
+xcopy /Y *.exe dist >>build.log
+xcopy /Y *.dll dist >>build.log
+xcopy /Y %~dp0\addons.ps1 dist >>build.log
+cd dist
+ren *.dot *.ps1 >>build.log
+cd ..
+echo [dot.build] End build.log.>>build.log
+echo [dot] Build finished, check %cd%\build.log for the full log. (ERRORCODE: !ERRORLEVEL!)
+exit /b !ERRORLEVEL!
+
+:add
+%~dp0\jq -r -c ".packages" "dot.json" >temp
+set /p PACKAGES=<temp
+del temp
+set PACKAGES=%PACKAGES:[=%
+set PACKAGES=%PACKAGES:]=%
+echo {"dotv":"%version%", "name":"mydot", "v": "0.1", "packages": [%PACKAGES%, "%2"]} >dot2.json
+%~dp0\jq -r -s -c add "dot2.json" "dot.json" >dot.json
+del dot2.json
+echo [dot] Package has been added to `dot.json` with error code (!ERRORLEVEL!)
+exit /b !ERRORLEVEL!
---
+++ dot.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+#    __  __             _                    _  _    _        ____          _     _____  _       _____ 
+#   |  \/  |           | |                  (_)| |  | |      |  _ \        | |   / ____|| |     |_   _|
+#   | \  / |  __ _   __| |  ___   __      __ _ | |_ | |__    | |_) |  __ _ | |_ | |     | |       | |  
+#   | |\/| | / _` | / _` | / _ \  \ \ /\ / /| || __|| '_ \   |  _ <  / _` || __|| |     | |       | |  
+#   | |  | || (_| || (_| ||  __/   \ V  V / | || |_ | | | |  | |_) || (_| || |_ | |____ | |____  _| |_ 
+#   |_|  |_| \__,_| \__,_| \___|    \_/\_/  |_| \__||_| |_|  |____/  \__,_| \__| \_____||______||_____|
+#                                                                                                      
+#                                                                                                      
+
+# dotlang, (C) 2024 NEOAPPS
+# This code is licensed under MIT license (see LICENSE.txt for details)
+
+version=1
+script_dir="$(dirname "$(realpath "$0")")"
+
+function print_help {
+    echo "dotCLI - The CLI for dotlang."
+    echo "Current dot Version Installed: dot v$version."
+    echo
+    echo "Usage:"
+    echo -e "\\033[4mdot\\033[0m --help, -h, /?          Show help Page"
+    echo -e "\\033[4mdot\\033[0m build                Build Project in the current directory"
+    echo -e "\\033[4mdot\\033[0m new                  Make a project in the current directory"
+    echo -e "\\033[4mdot\\033[0m add PACKAGE          Adds PACKAGE to the current project"
+}
+
+function create_new_project {
+    if [ -f "$PWD/dot.json" ]; then
+        echo -e "\\033[1;31mWARNING: PROJECT IN THIS DIRECTORY ALREADY EXISTS. IF YOU WANT TO OVERRIDE IT, MAKE A FILE WITH NAME \"override.txt\".\\033[0m"
+        if [ -f "override.txt" ]; then
+            echo "{\"dotv\":\"$version\", \"name\":\"mydot\", \"v\": \"0.1\", \"packages\": []}" >dot.json
+            rm -f override.txt
+            echo "Project with name 'mydot' has been made in $PWD."
+        fi
+    else
+        echo "{\"dotv\":\"$version\", \"name\":\"mydot\", \"v\": \"0.1\", \"packages\": []}" >dot.json
+		echo "Import-Module .\addons.ps1 # Enable dotlang">main.dot
+        echo "Project with name 'mydot' has been made in $PWD."
+    fi
+}
+
+function build_project {
+    echo "[dot.build] Reinitialized build.log file." >build.log
+    echo "[dot] Building project from $PWD..."
+    echo "[dot] Building project from $PWD..." >>build.log
+    sleep 1
+    dotv=$(jq -r .dotv dot.json)
+    if [ "$version" -lt "$dotv" ]; then
+        echo "[dot] You're using an outdated version of dot. please install dot $dotv or more"
+        exit 1
+    fi
+    echo "[dot] Getting the latest packages from $PWD/dot.json..."
+    echo "[dot] Getting the latest packages from $PWD/dot.json..." >>build.log
+    packages=$(jq -r ".packages[]" dot.json)
+    for child in $packages; do
+        echo "[dot] Running: ship install $child"
+        echo "[dot] Running: ship install $child" >>build.log
+        echo "[dot] Begin 'ship install $child' log.." >>build.log
+        ship install "$child" >>build.log
+        echo "[dot] End 'ship install $child' log.." >>build.log
+        sleep 2
+    done
+    echo "[dot] Building project..."
+    echo "[dot] Building project..." >>build.log
+    rm -rf dist
+    mkdir dist >>build.log
+    cp *.dot dist/ >>build.log
+    cp *.exe dist/ >>build.log
+    cp *.dll dist/ >>build.log
+    cp $script_dir/addons.ps1 dist/ >>build.log
+    cd dist || exit
+    for f in *.dot; do mv -- "$f" "${f%.dot}.ps1"; done
+    cd ..
+    echo "[dot.build] End build.log." >>build.log
+    echo "[dot] Build finished, check $PWD/build.log for the full log. (ERRORCODE: $?)"
+    exit $?
+}
+
+function add_package {
+    packages=$(jq -r -c ".packages" "dot.json")
+    packages="${packages#[}"
+    packages="${packages%]}"
+    echo "{\"dotv\":\"$version\", \"name\":\"mydot\", \"v\": \"0.1\", \"packages\": [$packages, \"$2\"]}" >dot2.json
+    jq -r -s -c add "dot2.json" "dot.json" >dot.json
+    rm -f dot2.json
+    echo "[dot] Package has been added to 'dot.json' with error code ($?)"
+    exit $?
+}
+
+case "$1" in
+    --help|-h|"/?")
+        print_help
+        ;;
+    build)
+        build_project
+        ;;
+    new)
+        create_new_project
+        ;;
+    add)
+        add_package "$@"
+        ;;
+    idk)
+        echo "it's alright. idk too hehe"
+        ;;
+    *)
+        echo "Invalid Syntax! use 'dot --help' for help."
+        ;;
+esac
