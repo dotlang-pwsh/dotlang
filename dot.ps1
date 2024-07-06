a531ddc54fcbb61f85117b7cb999696e2d7b2bb5
Asd22 Entertainment <158327205+neoapps-dev@users.noreply.github.com>
2024-07-06 18:00:11 +0300

    fix

--- dot.ps1
+++ dot.ps1
@@ -28,24 +28,55 @@ function Invoke-WebRequestSimple {
     Invoke-RestMethod -Uri $Url
 }
 
-function EncryptData {
-    param (
-        [string]$Data,
-        [string]$Password
-    )
-    $secureString = ConvertTo-SecureString -String $Password -AsPlainText -Force
-    $encryptedData = ConvertTo-EncryptedData -StringData $Data -SecureKey $secureString
-    return $encryptedData
+function Create-AesManagedObject($key, $IV) {
+    $aesManaged = New-Object "System.Security.Cryptography.AesManaged"
+    $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+    $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+    $aesManaged.BlockSize = 128
+    $aesManaged.KeySize = 256
+    if ($IV) {
+        if ($IV.getType().Name -eq "String") {
+            $aesManaged.IV = [System.Convert]::FromBase64String($IV)
+        }
+        else {
+            $aesManaged.IV = $IV
+        }
+    }
+    if ($key) {
+        if ($key.getType().Name -eq "String") {
+            $aesManaged.Key = [System.Convert]::FromBase64String($key)
+        }
+        else {
+            $aesManaged.Key = $key
+        }
+    }
+    $aesManaged
 }
 
-function DecryptData {
-    param (
-        [string]$EncryptedData,
-        [string]$Password
-    )
-    $secureString = ConvertTo-SecureString -String $Password -AsPlainText -Force
-    $decryptedData = ConvertFrom-EncryptedData -EncryptedData $EncryptedData -SecureKey $secureString
-    return $decryptedData
+function dotaes() {
+    $aesManaged = Create-AesManagedObject
+    $aesManaged.GenerateKey()
+    [System.Convert]::ToBase64String($aesManaged.Key)
+}
+
+function EncryptData($key, $unencryptedString) {
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($unencryptedString)
+    $aesManaged = Create-AesManagedObject $key
+    $encryptor = $aesManaged.CreateEncryptor()
+    $encryptedData = $encryptor.TransformFinalBlock($bytes, 0, $bytes.Length);
+    [byte[]] $fullData = $aesManaged.IV + $encryptedData
+    $aesManaged.Dispose()
+    [System.Convert]::ToBase64String($fullData)
+}
+
+function DecryptData($key, $encryptedStringWithIV) {
+    $bytes = [System.Convert]::FromBase64String($encryptedStringWithIV)
+    $IV = $bytes[0..15]
+    $aesManaged = Create-AesManagedObject $key $IV
+    $decryptor = $aesManaged.CreateDecryptor();
+    $unencryptedData = $decryptor.TransformFinalBlock($bytes, 16, $bytes.Length - 16);
+    $aesManaged.Dispose()
+    [System.Text.Encoding]::UTF8.GetString($unencryptedData).Trim([char]0)
 }
 
 function Invoke-InContext {
